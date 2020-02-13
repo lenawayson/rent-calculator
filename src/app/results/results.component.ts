0409cb8db3ac48bcaab096de5d6072dc4e7116ac
Lena Wayson <lena.wayson@gmail.com>
2020-02-13 14:48:47 -0700

    Add calculation logic

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+const TOTAL_RENT = 2520; 
+const ACTIVE_RESIDENTS = 3;
 
 @Component({
   selector: 'app-results',
@@ -8,21 +12,45 @@ import { Component, OnInit } from '@angular/core';
 export class ResultsComponent implements OnInit {
 
   roommates = [
-    {name: "Lena", id: 0, rent: 630, parking: 100, utilities: null, total: null},
-    {name: "Evan", id: 1, rent: 630, parking: 0, utilities: null, total: null},
-    {name: "Megan", id: 2, rent: 1000, parking: 100, utilities: null, total: null},
-    {name: "Chris", id: 4, rent: 250, parking: 0, utilities: null, total: null},
+    {name: "Lena", id: 0, rent: 630, parking: 100, utilities: null, owed: null},
+    {name: "Evan", id: 1, rent: 630, parking: 0, utilities: null, owed: null},
+    {name: "Megan", id: 2, rent: 1000, parking: 100, utilities: null, owed: null},
+    {name: "Chris", id: 4, rent: 250, parking: 0, utilities: 0, owed: null},
   ];
-  selected = this.roommates[0];
-  total = null;
+  selected = this.roommates[0]; //initial selection
+  totalDue = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams
+      .subscribe(params => {
+        this.totalDue = params.q;
+      });
+    this.calculateOwed();
   }
 
-  onClick(id){
-    this.selected = this.roommates[id]
+  select(id){
+    this.selected = this.roommates[id];
+  }
+
+  calculateOwed(){
+    let misc = this.totalDue - TOTAL_RENT;
+    let utils = misc - this.calculateTotalParking();
+    this.roommates.forEach(x => {
+      if (x.utilities === null){
+        x.utilities = parseFloat((utils/ACTIVE_RESIDENTS).toFixed(2));
+      }
+      x.owed = x.rent + x.utilities + x.parking;
+    })
+  }
+
+  calculateTotalParking(){
+    let sum = 0;
+    this.roommates.forEach(x => {
+      sum += x.parking;
+    })
+    return sum;
   }
 
 }
